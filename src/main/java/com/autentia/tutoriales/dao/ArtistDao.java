a2eb69b69ae2a6c187c39f726e9fffc5d53c0a00
Yair Segura <ysegura@autentia.com>
2016-04-25 15:51:45 +0200

    fix

---
+++ src/main/java/com/autentia/tutoriales/dao/ArtistDao.java
@@ -0,0 +1,32 @@
+package com.autentia.tutoriales.dao;
+
+import com.autentia.tutoriales.model.Artist;
+
+import java.util.List;
+
+public class ArtistDao implements Dao<Artist> {
+
+    @Override public List<Artist> getAll() {
+        return null;
+    }
+
+    @Override public Artist getById(Long id) {
+        return null;
+    }
+
+    @Override public void delete(Long id) {
+
+    }
+
+    @Override public void delete(Artist entity) {
+
+    }
+
+    @Override public void insert(Artist entity) {
+
+    }
+
+    @Override public void update(Long id, Artist entity) {
+
+    }
+}
---
+++ src/main/java/com/autentia/tutoriales/model/Artist.java
@@ -0,0 +1,8 @@
+package com.autentia.tutoriales.model;
+
+public class Artist {
+
+    private Long id;
+    private String name;
+
+}
